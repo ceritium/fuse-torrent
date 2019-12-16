001c6345f602d8a1a9a50c4adeab7534df7aae38
Jose Galisteo <ceritium@gmail.com>
2019-12-16 12:38:03 +0100

    Only mount a single dir

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-const prettysize = require('prettysize')
 const fuse = require('fuse-bindings')
 const sqlite = require('sqlite')
 const fs = require('fs')
@@ -128,64 +127,18 @@ function mountTorrents () {
   cache = fs.realpathSync(cache)
   console.log(cache)
 
-  let id = 0
-
-  var torrents = []
-
-  async function start () {
-    const db = await sqlite.open(dbFile)
-    const items = await db.all('SELECT * FROM torrents where id > ?', id)
-
-    items.forEach(item => {
-      torrents.push(item)
-      id = item.id
-      const events = drive(item, mount, cache)
-      events.on('mount', source => console.log('Mounted ' + source.mnt))
-      events.on('start', source => console.log('Swarm starting ' + source.mnt))
-      events.on('ready', source => console.log('Swarm ready ' + source.mnt))
-      events.on('stop', source => console.log('Stop swarm' + source.mnt))
-      events.on('download', index => {
-        const down = prettysize(events.engine.swarm.downloaded)
-        const downSpeed = prettysize(events.engine.swarm.downloadSpeed()).replace('Bytes', 'b') + '/s'
-
-        const notChoked = function (result, wire) {
-          return result + (wire.peerChoking ? 0 : 1)
-        }
-        const connects = events.engine.swarm.wires.reduce(notChoked, 0) + '/' + events.engine.swarm.wires.length + ' peers'
-
-        console.log('Downloaded ' + connects + ' : ' + downSpeed + ' : ' + down + ' of ' + prettysize(events.engine.torrent.length) + ' for ' + item.name + ' : ' + index)
-      })
-    })
-  }
-
-  start()
-  var checkNewTorrentsInterval = setInterval(start, 10000)
-
-  function unmount (index) {
-    const item = torrents[index]
-
-    if (item) {
-      const mnt = path.join(mount, path.resolve('/', item.name))
-      console.log('Unmounting ' + mnt)
-
-      fuse.unmount(mnt, function () {
-        fs.rmdir(mnt, function () {
-          unmount(index + 1)
-        })
-      })
-    } else {
-      process.exit()
-    }
-  }
-
+  drive(dbFile, mount, cache)
   var exit = async function () {
     console.log('\n')
-    clearInterval(checkNewTorrentsInterval)
     setTimeout(process.kill.bind(process, process.pid), 2000).unref()
     process.removeListener('SIGINT', exit)
     process.removeListener('SIGTERM', exit)
 
-    unmount(0)
+    fuse.unmount(mount, function () {
+      fs.rmdir(mount, function () {
+        process.exit()
+      })
+    })
   }
 
   process.on('SIGINT', exit)
